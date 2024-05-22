fc860de9024084dafc8e178259f8137baf597611
Cesar David Millan <cesardavidmillan5@gmail.com>
2024-05-21 19:12:05 -0500

    working on bank account and transactions controllers

--- pagos/src/main/java/com/bancolombia/pagos/controller/UserController.java
+++ pagos/src/main/java/com/bancolombia/pagos/controller/UserController.java
@@ -1,4 +1,54 @@
 package com.bancolombia.pagos.controller;
 
+import com.bancolombia.pagos.model.User;
+import com.bancolombia.pagos.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/api/v1/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User entity = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(entity);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        User user = userService.getUser(id);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<User>> getUsers() {
+        Iterable<User> users = userService.getUsers();
+        return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User updatedUser) {
+        User user = userService.getUser(id);
+        if (user != null) {
+            user.setName(updatedUser.getName());
+            return ResponseEntity.ok(userService.createUser(user));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
